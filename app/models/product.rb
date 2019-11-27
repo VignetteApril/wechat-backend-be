7a1d3268d9c34e64ba84de5dd46dc12b4d0d6b29
zhangjie <zhangjieboot@gmail.com>
2019-11-27 10:54:08 +0800

    change migrate

--- app/models/product.rb
+++
@@ -1,7 +0,0 @@
-class Product < ApplicationRecord
-  has_one_attached :small_image
-  has_one_attached :normal_image
-  has_one_attached :big_image
-
-  has_many :app_images, dependent: :destroy
-end
--- db/migrate/20190719074921_rename_type_to_app_image.rb
+++
@@ -1,5 +0,0 @@
-class RenameTypeToAppImage < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :app_images, :type, :usage_type
-  end
-end
