764a79bee7ea4b9e3df03bf3c7d02263523c5dce
zhangjie <zhangjieboot@gmail.com>
2019-11-27 14:48:15 +0800

    change the pumactrl.sh

--- pumactrl.sh
+++ pumactrl.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+share_puma_path="/root/deploy_apps/youshule/shared/config/puma.rb"
+share_puma_pid_path="/root/deploy_apps/youshule/shared/tmp/pids/puma.pid"
+
 case "$1" in
     "")
         echo -n "please provide a parameter (start, stop, reload)."
         ;;
     start)
         echo -n "starting puma..."
-        bundle exec puma -C /data/applications/pantrysbest-cacaoh-backend/shared/config/puma.rb
+        bundle exec puma -C $share_puma_path
         ;;
     stop)
         echo "stoping puma..."
-        kill `cat "/root/deploy_apps/youshule/shared/tmp/pids/puma.pid"`
+        kill `cat "${share_puma_pid_path}"`
         ;;
     reload)
         echo "reloading puma..."
-        kill `cat "/root/deploy_apps/youshule/shared/tmp/pids/puma.pid"`
-        bundle exec puma -C /data/applications/pantrysbest-cacaoh-backend/shared/config/puma.rb
+        kill `cat "${share_puma_pid_path}"`
+        bundle exec puma -C $share_puma_path
         ;;
 esac
