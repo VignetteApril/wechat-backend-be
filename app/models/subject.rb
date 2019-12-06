0474caeaf9f9097a40836eeeb298db0575f3b7b9
zhangjie <zhangjieboot@gmail.com>
2019-12-06 22:09:45 +0800

    add subject img

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,6 +3,7 @@ class Subject < ApplicationRecord
   belongs_to :grade
   has_many :courses, dependent: :destroy
   has_many :book_codes, dependent: :destroy
+  has_one_attached :img
 
   # validates
   validates :code_prefix, length: { is: 2 }, uniqueness: { case_sensitive: false }, format: { with: /\b[A-Z][A-Z]+\b/, message: '只支持大写字母' }, all_blank: false
